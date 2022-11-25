caa9982eaca69691cb0e182be63d1a68e04a3644
Marcelo Norberto <norbertomarcelo8@gmail.com>
2022-11-24 21:17:43 -0300

    Comecando curso de react-router-dom

---
+++ React-desenvolvendo-em-React-Router-com-JavaScript/ola-mundo/src/routes.js
@@ -0,0 +1,19 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Menu from './componentes/Menu';
+import Inicio from './paginas/Inicio';
+import SobreMim from './paginas/SobreMim';
+
+export default function AppRoutes() {
+  return (
+    <BrowserRouter>
+      <Menu />
+
+      <Routes>
+        <Route path='/' element={<Inicio />} />
+        <Route path='/sobremim' element={<SobreMim />} />
+        <Route path='*' element={<div>Página não encontrada</div>} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
